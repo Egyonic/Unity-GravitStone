72d7e3f57188885e80440c08152466bcd6296197
Egyonic <787961091@qq.com>
2020-06-25 16:07:01 +0800

    小修改

---
+++ Assets/Script/Test/AutoMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMove : MonoBehaviour
+{
+    public int speed;
+    // Start is called before the first frame update
+    private Rigidbody2D rigidbody2D;
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 left = new Vector3(-1,0);
+        //Vector3 le = left + transform.position;
+        //Vector3 left = new Vector2(-speed, 0);
+        //rigidbody2D.MovePosition(transform.position + left*Time.deltaTime);
+        //transform.position = Vector2.MoveTowards(transform.position, le, speed * Time.deltaTime);
+    }
+
+    private void FixedUpdate() {
+        Vector3 left = new Vector2(-speed, 0);
+        rigidbody2D.MovePosition(transform.position + left * Time.deltaTime);
+    }
+}
