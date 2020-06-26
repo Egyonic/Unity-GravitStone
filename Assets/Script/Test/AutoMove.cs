736055fe4adcb866cb8d87a228fbeb8bb1928e6a
Egyonic <787961091@qq.com>
2020-06-26 11:39:53 +0800

    区域模拟重力移动管理prefeb,GravityAreaController

--- Assets/Script/Test/AutoMove.cs
+++ Assets/Script/Test/AutoMove.cs
@@ -4,26 +4,37 @@ using UnityEngine;
 
 public class AutoMove : MonoBehaviour
 {
-    public int speed;
-    // Start is called before the first frame update
+    // 两个自身的移动速度，将由管理模拟重力区域的父类来控制
+    // 在值中通过正负值来控制方向
+    private float speedX;  //  x速度
+    private float speedY;  //  y速度
+
+    private GravityAreaController gravityAreaController;
     private Rigidbody2D rigidbody2D;
+    // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        gravityAreaController = transform.GetComponentInParent<GravityAreaController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Vector3 left = new Vector3(-1,0);
-        //Vector3 le = left + transform.position;
-        //Vector3 left = new Vector2(-speed, 0);
-        //rigidbody2D.MovePosition(transform.position + left*Time.deltaTime);
-        //transform.position = Vector2.MoveTowards(transform.position, le, speed * Time.deltaTime);
+        //Debug.Log(gravityAreaController.speedX);
     }
 
     private void FixedUpdate() {
-        Vector3 left = new Vector2(-speed, 0);
+        syncSpeedAndDireaction();
+        Vector3 left = new Vector2(speedX, speedY);
         rigidbody2D.MovePosition(transform.position + left * Time.deltaTime);
+
     }
+
+    private void syncSpeedAndDireaction() {
+        speedX = gravityAreaController.getSpeedX();
+        speedY = gravityAreaController.getSpeedY();
+    }
+
+  
 }
---
+++ Assets/Script/Test/GravityAreaController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ 用于控制一个区域内的模拟改变重力和移动的效果
+     */
+
+public class GravityAreaController : MonoBehaviour
+{
+    public float speedX;
+    public float speedY;
+    public Vector2 direction;   //控制区域内的移动方向
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public float getSpeedX() {
+        return speedX * direction.x;
+    }
+
+    public float getSpeedY() {
+        return speedY * direction.y;
+    }
+}
