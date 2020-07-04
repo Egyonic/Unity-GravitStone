c5dbbde242f47202fa5e5b896d19e11134139310
Egyonic <787961091@qq.com>
2020-07-04 19:29:52 +0800

    素材替换摆放2

--- Assets/Script/CenterRoom.cs
+++ Assets/Script/CenterRoom.cs
@@ -6,16 +6,26 @@ public class CenterRoom : MonoBehaviour
 {
     public Transform collider1; //第一个和第二个用来挡住的碰撞体
     public Transform collider2;
-   
     public Vector3 move;
+
+    //三个触发器的物体对象，用来获取SpiteRender对象
+    public Transform[] StatueTriggers;  
+
+    //暂时没有用
     public bool[] RoomTriggers;
+
     private Rigidbody2D rigidbody2D;
+    private SpriteRenderer[] statueRenderers;
 
     // Start is called before the first frame update
     void Start()
     {
         //move = new Vector3(0, -1, 0);
         RoomTriggers = new bool[3];
+        statueRenderers = new SpriteRenderer[3];
+        for(int i = 0; i < 3; i++) {
+            statueRenderers[i] = StatueTriggers[i].GetComponent<SpriteRenderer>();
+        }
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
@@ -37,5 +47,9 @@ public class CenterRoom : MonoBehaviour
     public void destroyCollider2() {
         if (collider2 != null)
             Destroy(collider2.gameObject); //消除第二个
+    }
+
+    public void setStatueVisible(int index) {
+        statueRenderers[index].enabled = true;
     }
 }
--- Assets/Script/CenterRoomTrigger.cs
+++ Assets/Script/CenterRoomTrigger.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CenterRoomTrigger : MonoBehaviour
 {
+    //public int triggerId;
     private CenterRoom centerRoom;  //中间房间的CenterRoom脚本对象
     public static PlayerController playerController;
 
@@ -23,18 +24,27 @@ public class CenterRoomTrigger : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if(collision.gameObject.CompareTag("Player") && collision.GetType().ToString() == "UnityEngine.CapsuleCollider2D") {
+            
             //检查玩家是否有拿到石像物品（触发器），三个都有的情况
             if(playerController.CenterRoomTrigger[0] && playerController.CenterRoomTrigger[1]
                 && playerController.CenterRoomTrigger[2]) {
-                centerRoom.RoomTriggers[0] = true;
-                centerRoom.RoomTriggers[1] = true;
-                centerRoom.RoomTriggers[2] = true;
                 centerRoom.destroyCollider2();  //房间下移到最下方
             }
             //只有第一个中间的触发器的情况
-            else if (playerController.CenterRoomTrigger[0]) {
+            if (playerController.CenterRoomTrigger[0]) {
                 centerRoom.RoomTriggers[0] = true;
                 centerRoom.destroyCollider1();  //房间下移到中间
+                centerRoom.setStatueVisible(0);
+            }
+
+            if (playerController.CenterRoomTrigger[1]) {
+                centerRoom.RoomTriggers[1] = true;
+                centerRoom.setStatueVisible(1);
+            }
+
+            if (playerController.CenterRoomTrigger[2]) {
+                centerRoom.RoomTriggers[2] = true;
+                centerRoom.setStatueVisible(2);
             }
         }
     }
